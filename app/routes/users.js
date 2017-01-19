77775894f9ca0cedc4f41d31a2d1864189a80aab
mcpengelly <pengelly.mat@gmail.com>
2017-01-18 21:03:37 -0500

    user POST endpoint

--- app/routes/users.js
+++ app/routes/users.js
@@ -10,4 +10,23 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.post('/', function(req, res, next){
+	var db = req.db;
+	console.log(req)
+	var username = req.query.username;
+	var password = req.query.password;
+	var email = req.query.email;
+
+	var collection = db.get('userlist');
+	var user = {
+		username: username,
+		password: password,
+		email: email
+	};
+
+	collection.insert(user);
+
+	res.send('Successfully inserted:', user);
+});
+
 module.exports = router;
