75a9573e6142302619ed5167c0ca78814c30f669
mcpengelly <pengelly.mat@gmail.com>
2017-01-18 21:12:09 -0500

    delete all users

--- app/routes/users.js
+++ app/routes/users.js
@@ -12,7 +12,6 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next){
 	var db = req.db;
-	console.log(req)
 	var username = req.query.username;
 	var password = req.query.password;
 	var email = req.query.email;
@@ -28,5 +27,12 @@ router.post('/', function(req, res, next){
 
 	res.send('Successfully inserted:', user);
 });
+
+router.delete('/', function(req, res, next){
+	var db = req.db;
+	var collection = db.get('userlist');
+	collection.remove({});
+	res.send('cleared all users')
+})
 
 module.exports = router;
