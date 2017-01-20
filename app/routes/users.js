0d53db261d93f13b787eaff122f15cfe273e094b
mcpengelly <pengelly.mat@gmail.com>
2017-01-19 19:54:24 -0500

    add random-user endpoint, single user lookup by id

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var catnames = require('cat-names');
+var count = 0;
 
-/* GET users listing. */
+// access all users
 router.get('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('userlist');
@@ -10,14 +12,26 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// access a user via its id
+router.get('/:id', function(req, res, next){
+	var db = req.db;
+	var collection = db.get('userlist');
+
+	collection.find({ id: req.query.id }, {}, function(err, documents){
+		res.send(documents);
+	});
+});
+
+// create a user, optionally pass username, password, email
 router.post('/', function(req, res, next){
 	var db = req.db;
-	var username = req.query.username;
-	var password = req.query.password;
-	var email = req.query.email;
+	var username = req.query.username || '';
+	var password = req.query.password || '';
+	var email = req.query.email || '';
 
 	var collection = db.get('userlist');
 	var user = {
+		id: count,
 		username: username,
 		password: password,
 		email: email
@@ -25,14 +39,35 @@ router.post('/', function(req, res, next){
 
 	collection.insert(user);
 
+	count++;
 	res.send('Successfully inserted:', user);
 });
 
+// creates a random user
+router.post('/random-user', function(req, res, next){
+	var db = req.db;
+	var username = catnames.random();
+	var password = 'test';
+
+	var collection = db.get('userlist');
+	var user = {
+		id: count,
+		username: username,
+		password: password
+	};
+
+	collection.insert(user);
+
+	count++;
+	res.send('Successfully inserted:', user);
+});
+
+// clears the database
 router.delete('/', function(req, res, next){
 	var db = req.db;
 	var collection = db.get('userlist');
 	collection.remove({});
 	res.send('cleared all users')
-})
+});
 
 module.exports = router;
